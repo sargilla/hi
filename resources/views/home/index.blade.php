caf6332aa4b26d85544924dde44a20b2fb58b9bd
Santiago Argilla <sargilla@gmail.com>
2021-09-03 08:41:24 -0300

    en produccion

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,25 @@
 @extends('plantilla::layout')
 @section('header_title', 'INICIO')
 @section('main-content')
-        @include('home.bienvenida')
-        @include('home.actualidad')
-        @include('home.datos_impacto')     
+@include('home.bienvenida')
+@include('home.actualidad')
+@include('home.datos_impacto')
 @endsection
+
+@push('extra_scripts')
+<script>
+    jQuery( window ).load(function() {
+    // Run code
+    window.matchMedia('screen and (min-resolution: 2dppx)')
+        .addEventListener("change", function(e) {
+            console.log(window.devicePixelRatio,e)
+        if (e.matches) {
+        /* devicePixelRatio >= 2 */
+        } else {
+        /* devicePixelRatio < 2 */
+        }
+
+    });
+    });
+</script>
+@endpush
