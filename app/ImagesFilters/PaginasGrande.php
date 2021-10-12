f63ae761d12af45060b13e681e3419f1eb8db66d
Santiago Argilla <sargilla@gmail.com>
2021-10-12 15:55:53 -0300

    resolucion imagenes

--- app/ImagesFilters/PaginasGrande.php
+++ app/ImagesFilters/PaginasGrande.php
@@ -10,9 +10,10 @@ class PaginasGrande implements FilterInterface
     public function applyFilter(Image $image)
     {
         // return $image->fit(760, 380);
-        return $image->resize(null, 380, function ($constraint) {
-		    $constraint->aspectRatio();
-		});
+        return $image->fit(500, 380);
+        // return $image->resize(null, 380, function ($constraint) {
+		//     $constraint->aspectRatio();
+		// });
         // return $image->resize(250, null, function ($constraint) {
         //     $constraint->aspectRatio();
         // })->resizeCanvas(250, 250, 'center', false);
