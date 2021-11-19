886d3e1f462bfe59b1dbad49354f5679ddbb52f8
Santiago Argilla <sargilla@gmail.com>
2021-11-19 14:32:14 -0300

    con favicon

--- resources/views/layout/hi/_htmlheader.blade.php
+++ resources/views/layout/hi/_htmlheader.blade.php
@@ -5,22 +5,25 @@
     <meta name="author" content="Estudio Rompecabezas && 3tristestigres">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{--  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/apple-touch-icon.png')}}">
-    <meta name="keyword" content="">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png')}}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon/android-chrome-192x192') }}">
-    <link rel="manifest" href="{{ asset('manifest.webmanifest')}}">
-    <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg')}}" color="#333333">
-    <meta name="msapplication-TileColor" content="#333333">
-    <meta name="theme-color" content="#333333">
+    <meta name="keyword" content="accesibilidad, naciones unidas">
 
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest')}}">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg')}}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffffff">
+
+    {{--
     <meta property="og:image:width" content="279">
     <meta property="og:image:height" content="279">
     <meta property="og:title" content="Cablecord">
     <meta property="og:description" content="Materiales El&eacute;ctricos">
     <meta property="og:url" content="https://www.cablecordrosario.com.ar/">
-    <meta property="og:image" content="https://www.cablecordrosario.com.ar/og-image.jpg"> --}}
+    <meta property="og:image" content="https://www.cablecordrosario.com.ar/og-image.jpg">
+    --}}
 
     <title>@yield('header_title', 'Titulo de la Pagina') </title>
 
